26292163705cb28e72778bb281b5805ca055230c
Cibi211 <cibisadhana@gmail.com>
2026-03-31 21:50:49 +0530

    Added AI files

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,214 @@
+# import tldextract
+# from homograph_detector import detect_homograph
+# from phishing_detector import detect_phishing
+# from upi_detector import detect_upi_scam
+# from domain_similarity import detect_typosquatting
+# from ml_phishing_detector import predict_phishing
+# from payment_qr_detector import detect_fake_payment_qr
+# from deep_translator import GoogleTranslator
+
+# # --------------------------
+# # Translation Functions
+# # --------------------------
+
+# def translate_text(text, lang):
+
+#     lang_map = {
+#         "en": "en",
+#         "ta": "ta",
+#         "tamil": "ta",
+#         "hi": "hi",
+#         "hindi": "hi"
+#     }
+
+#     target = lang_map.get(lang.lower(), "en")
+
+#     if target == "en":
+#         return text
+
+#     try:
+#         translated = GoogleTranslator(source="auto", target=target).translate(text)
+#         return translated
+#     except Exception as e:
+#         print("Translation error:", e)
+#         return text
+
+
+# def translate_response(result, lang):
+
+#     for exp in result["explanations"]:
+#         exp["reason"] = translate_text(exp["reason"], lang)
+#         exp["module"] = translate_text(exp["module"], lang)
+
+#     risk_map = {
+#         "ta": {
+#             "HIGH": "அதிக அபாயம்",
+#             "MEDIUM": "மிதமான அபாயம்",
+#             "LOW": "குறைந்த அபாயம்"
+#         },
+#         "hi": {
+#             "HIGH": "उच्च जोखिम",
+#             "MEDIUM": "मध्यम जोखिम",
+#             "LOW": "कम जोखिम"
+#         }
+#     }
+#     if "feature_importance" in exp:
+#             exp["feature_importance"] = [
+#                 translate_text(f, lang) for f in exp["feature_importance"]
+#             ]
+
+#     key = lang.lower()
+
+#     if key in risk_map:
+#         result["risk_level"] = risk_map[key].get(
+#             result["risk_level"], result["risk_level"]
+#         )
+
+#     return result
+
+
+# def analyze_qr_data(data, language="en"):
+#     print("LANG IN ANALYZER:", language)
+
+#     risk_score = 0
+#     explanations = []
+
+#     # --------------------------
+#     # Clean input
+#     # --------------------------
+#     data = data.strip()
+
+#     if not data.startswith(("http://", "https://", "upi://")):
+#         data = "http://" + data
+
+#     ext = tldextract.extract(data)
+#     domain = ext.domain + "." + ext.suffix
+
+#     # --------------------------
+#     # Homograph detection
+#     # --------------------------
+#     flag, reason = detect_homograph(data)
+
+#     if flag:
+#         risk_score += 50
+#         explanations.append({
+#             "module": "Homograph Detector",
+#             "reason": reason
+#         })
+
+#     # --------------------------
+#     # Typosquatting detection
+#     # --------------------------
+#     flag, reason = detect_typosquatting(domain)
+
+#     if flag:
+#         risk_score += 40
+#         explanations.append({
+#             "module": "Typosquatting Detector",
+#             "reason": reason
+#         })
+
+#     # --------------------------
+#     # Shortened URL detection
+#     # --------------------------
+#     SHORT_DOMAINS = ["bit.ly", "tinyurl.com", "t.co"]
+
+#     for short in SHORT_DOMAINS:
+#         if short in data:
+#             risk_score += 40
+#             explanations.append({
+#                 "module": "URL Shortener Detector",
+#                 "reason": "Shortened URL detected (common phishing technique)"
+#             })
+
+#     # --------------------------
+#     # UPI scam detection
+#     # --------------------------
+#     flag, reasons = detect_upi_scam(data)
+
+#     if flag:
+#         risk_score += 40
+#         for r in reasons:
+#             explanations.append({
+#                 "module": "UPI Scam Detector",
+#                 "reason": r
+#             })
+
+#     # --------------------------
+#     # Fake payment QR detection
+#     # --------------------------
+#     flag, reasons = detect_fake_payment_qr(data)
+
+#     if flag:
+#         risk_score += 50
+#         for r in reasons:
+#             explanations.append({
+#                 "module": "Payment QR Detector",
+#                 "reason": r
+#             })
+
+#     # --------------------------
+#     # HTTP security check
+#     # --------------------------
+#     if data.startswith("http://"):
+#         risk_score += 10
+#         explanations.append({
+#             "module": "Security Check",
+#             "reason": "URL uses insecure HTTP"
+#         })
+
+    
+
+#     # --------------------------
+#     # Phishing feature detection
+#     # --------------------------
+#     score, reasons = detect_phishing(data)
+
+#     risk_score += score
+
+#     for r in reasons:
+#         explanations.append({
+#             "module": "Phishing Feature Detector",
+#             "reason": r
+#         })
+
+   
+#     ml_flag, prob, shap_exp = predict_phishing(data)
+
+#     if ml_flag:
+
+#         risk_score += 30
+        
+
+#         explanations.append({
+#             "module": "ML Phishing Model",
+#             "reason": f"Predicted phishing with probability {prob:.2f}",
+#             "feature_importance": shap_exp
+#         })
+
+#     # --------------------------
+#     # Final risk calculation
+#     # --------------------------
+#     risk_score = min(risk_score, 100)
+
+#     if risk_score >= 70:
+#         level = "HIGH"
+#     elif risk_score >= 40:
+#         level = "MEDIUM"
+#     else:
+#         level = "LOW"
+
+#     result = {
+#     "risk_score": risk_score,
+#     "risk_level": level,
+#     "explanations": explanations
+# }
+
+# # 🔥 APPLY TRANSLATION HERE
+#     result = translate_response(result, language)
+
+#     return result
+
 import tldextract
 from homograph_detector import detect_homograph
 from phishing_detector import detect_phishing
@@ -5,9 +216,70 @@ from upi_detector import detect_upi_scam
 from domain_similarity import detect_typosquatting
 from ml_phishing_detector import predict_phishing
 from payment_qr_detector import detect_fake_payment_qr
+from deep_translator import GoogleTranslator
+
+# --------------------------
+# Translation Functions
+# --------------------------
+
+def translate_text(text, lang):
+
+    lang_map = {
+        "en": "en",
+        "ta": "ta",
+        "tamil": "ta",
+        "hi": "hi",
+        "hindi": "hi"
+    }
+
+    target = lang_map.get(lang.lower(), "en")
+
+    if target == "en":
+        return text
+
+    try:
+        translated = GoogleTranslator(source="auto", target=target).translate(text)
+        return translated
+    except Exception as e:
+        print("Translation error:", e)
+        return text
 
 
-def analyze_qr_data(data):
+def translate_response(result, lang):
+
+    for exp in result["explanations"]:
+        exp["reason"] = translate_text(exp["reason"], lang)
+        exp["module"] = translate_text(exp["module"], lang)
+
+    risk_map = {
+        "ta": {
+            "HIGH": "அதிக அபாயம்",
+            "MEDIUM": "மிதமான அபாயம்",
+            "LOW": "குறைந்த அபாயம்"
+        },
+        "hi": {
+            "HIGH": "उच्च जोखिम",
+            "MEDIUM": "मध्यम जोखिम",
+            "LOW": "कम जोखिम"
+        }
+    }
+    if "feature_importance" in exp:
+            exp["feature_importance"] = [
+                translate_text(f, lang) for f in exp["feature_importance"]
+            ]
+
+    key = lang.lower()
+
+    if key in risk_map:
+        result["risk_level"] = risk_map[key].get(
+            result["risk_level"], result["risk_level"]
+        )
+
+    return result
+
+
+def analyze_qr_data(data, language="en"):
+    print("LANG IN ANALYZER:", language)
 
     risk_score = 0
     explanations = []
@@ -96,6 +368,8 @@ def analyze_qr_data(data):
             "reason": "URL uses insecure HTTP"
         })
 
+    
+
     # --------------------------
     # Phishing feature detection
     # --------------------------
@@ -109,17 +383,7 @@ def analyze_qr_data(data):
             "reason": r
         })
 
-    # --------------------------
-    # ML phishing detection
-    # --------------------------
-    # ml_flag, prob = predict_phishing(data)
-
-    # if ml_flag:
-    #     risk_score += 30
-    #     explanations.append({
-    #         "module": "ML Phishing Model",
-    #         "reason": f"Model predicted phishing with probability {prob:.2f}"
-    #     })
+   
     ml_flag, prob, shap_exp = predict_phishing(data)
 
     if ml_flag:
@@ -145,8 +409,13 @@ def analyze_qr_data(data):
     else:
         level = "LOW"
 
-    return {
-        "risk_score": risk_score,
-        "risk_level": level,
-        "explanations": explanations
-    }
+    result = {
+    "risk_score": risk_score,
+    "risk_level": level,
+    "explanations": explanations
+}
+
+# 🔥 APPLY TRANSLATION HERE
+    result = translate_response(result, language)
+
+    return result
--- main.py
+++ main.py
@@ -1,16 +1,99 @@
-from fastapi import FastAPI, UploadFile, File
+# # from fastapi import FastAPI, UploadFile, File
+# # from qr_decoder import decode_qr
+# # from analyzer import analyze_qr_data
+
+# # app = FastAPI(
+# #     title="AI QR Phishing Detector",
+# #     description="Detects Quishing attacks in India",
+# #     version="1.0"
+# # )
+
+
+# # @app.post("/scan_qr")
+# # async def scan_qr(file: UploadFile = File(...)):
+
+# #     image_bytes = await file.read()
+
+# #     qr_data = decode_qr(image_bytes)
+
+# #     if not qr_data:
+# #         return {"error": "QR code not detected"}
+
+# #     return {
+# #         "decoded_data": qr_data
+# #     }
+
+
+# # @app.post("/analyze_qr")
+# # async def analyze_qr(data: str):
+
+# #     result = analyze_qr_data(data)
+
+# #     return result
+
+
+# from fastapi import FastAPI, UploadFile, File
+# from qr_decoder import decode_qr
+# from analyzer import analyze_qr_data
+# from fastapi.middleware.cors import CORSMiddleware
+
+# app = FastAPI(
+# #     title="AI QR Phishing Detector",
+# #     description="Detects Quishing attacks in India",
+# #     version="1.0"
+# )
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+# @app.post("/scan_and_analyze")
+# async def scan_and_analyze(file: UploadFile = File(...)):
+
+#     image_bytes = await file.read()
+
+#     # Step 1: decode QR
+#     qr_data = decode_qr(image_bytes)
+
+#     if not qr_data:
+#         return {
+#             "error": "QR code not detected"
+#         }
+
+#     # Step 2: analyze decoded data
+#     result = analyze_qr_data(qr_data)
+
+#     # Step 3: return combined response
+#     return {
+#         "decoded_data": qr_data,
+#         "analysis": result
+#     }
+
+from fastapi import FastAPI, UploadFile, File, Form
 from qr_decoder import decode_qr
 from analyzer import analyze_qr_data
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="AI QR Phishing Detector",
-    description="Detects Quishing attacks in India",
-    version="1.0"
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-
-@app.post("/scan_qr")
-async def scan_qr(file: UploadFile = File(...)):
+@app.post("/scan_and_analyze")
+async def scan_and_analyze(
+    file: UploadFile = File(...),
+    language: str = Form("en")   # 🔥 FIX
+):
+    print("LANG RECEIVED:", language)
 
     image_bytes = await file.read()
 
@@ -19,14 +102,9 @@ async def scan_qr(file: UploadFile = File(...)):
     if not qr_data:
         return {"error": "QR code not detected"}
 
+    result = analyze_qr_data(qr_data, language)  # 🔥 PASS LANGUAGE
+
     return {
-        "decoded_data": qr_data
+        "decoded_data": qr_data,
+        "analysis": result
     }
-
-
-@app.post("/analyze_qr")
-async def analyze_qr(data: str):
-
-    result = analyze_qr_data(data)
-
-    return result
